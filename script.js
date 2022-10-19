aebd72d7979ecb3fa4e8479185f56dcff1886402
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-10-18 22:27:42 -0300

    Integração final. closes #4

---
+++ script.js
@@ -0,0 +1,42 @@
+const clearList = () => {
+  const currencyList = document.getElementById('currency-list');
+  currencyList.innerHTML = '';
+}
+
+const renderBaseCurrencyTitle = (base) => {
+  const baseTitle = document.getElementById('title-base');
+  baseTitle.innerHTML = `Valores referentes a: 1 ${base}`;
+};
+
+const renderRateItemList = (currency, value) => {
+  const currencyList = document.getElementById('currency-list');
+  const fixedValue = value.toFixed(2);
+  const li = document.createElement('li');
+  li.innerHTML = `<strong>${currency}:</strong> ${fixedValue}`;
+  currencyList.appendChild(li);
+};
+
+const renderRates = (rates) => {
+  const ratesEntries = Object.entries(rates);
+  ratesEntries.forEach(([ currency, value ]) => {
+    renderRateItemList(currency, value);
+  });
+};
+
+const handleSearchEvent = async () => {
+  const currencyElement = document.querySelector('#currency-input');
+  const currencyValue = currencyElement.value;
+  const object = await fetchExchangeRates(currencyValue);
+  clearList();
+  renderRates(object.rates);
+  renderBaseCurrencyTitle(object.base);
+};
+
+const setupHtmlElements = () => {
+  const searchButton = document.getElementById('search-button');
+  searchButton.addEventListener('click', handleSearchEvent);
+};
+
+window.onload = () => {
+  setupHtmlElements();
+};
